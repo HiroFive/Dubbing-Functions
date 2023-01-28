4e3b31438773d08f6dc9fdc11afa3958d74e4a54
Ivan Hirogive <vanohahj5555@gmail.com>
2023-01-28 17:52:22 +0200

    canged algoritm in wrapPeriods

--- functions.js
+++ functions.js
@@ -1,4 +1,29 @@
-function wrapPeriods(text){
-    console.log(text.replace(/\.(?=[^\s])/g, ".\n \n"));
+const elements = {
+  wrapPeriods: document.querySelector("#wrapPeriodsResult"),
+  submitTextAria: document.querySelector("#submitTextAria"),
+  textAria: document.querySelector("#textAria"),
+};
+
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement(
+    { pageLanguage: "en" },
+    "google_translate_element"
+  );
 }
-  
+
+function wrapPeriods(text) {
+  const sentences = text.split(".");
+  let newText = "";
+  for (let i = 0; i < sentences.length; i++) {
+    newText += sentences[i];
+    if (i !== sentences.length - 1) {
+      newText += ".</br> </br>";
+    }
+  }
+  elements.wrapPeriods.innerHTML = newText;
+}
+
+elements.submitTextAria.addEventListener("click", () => {
+  const text = elements.textAria.value;
+  wrapPeriods(text);
+});
